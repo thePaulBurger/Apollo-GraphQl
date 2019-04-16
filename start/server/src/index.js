51191aa5264e4de7f3941a5df6688c155aa7956e
Paul Burger <paul.burger11@gmail.com>
2019-04-16 08:09:33 +0200

    Schema and resolvers

---
+++ start/server/src/index.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server');
+const {typeDefs} = require('./schema');
+
+const  resolvers = require ('./resolvers');
+const { createStore } = require('./utils');
+
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  })
+});
+
+
+// const server = new ApolloServer(
+  // { typeDefs, 
+  //   resolvers}
+  // );
+
+
+
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+  });
+  
